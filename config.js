70e0a5b0a943f4e8142c8698fc245ca605ee0e16
Spexxerdark <152618060+Spexxerdark@users.noreply.github.com>
2023-12-01 18:50:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['91919098804458', 'ROLEX', true],
+  ['919098804458', 'VIP', true], 
   [''] 
 ] //Number of owners
 
